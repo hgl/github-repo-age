c2d6f13f5b81a69d824e34402edd1a0fc6697c00
Glen Huang <i@hgl.io>
2014-04-04 09:55:01 +0800

    Poll if being responsed with 202

--- github-repo-age.safariextension/github-repo-age.js
+++ github-repo-age.safariextension/github-repo-age.js
@@ -1,3 +1,25 @@
+function getRepoAge(url, callback) {
+	var xhr = new XMLHttpRequest();
+	xhr.onreadystatechange = function () {
+		if (xhr.readyState != 4) return;
+
+		if (xhr.status == 202) {
+				setTimeout(function () {
+						getRepoAge(url, callback);
+				}, 1000);
+				return;
+		}
+
+		if (xhr.status != 200 || !xhr.responseText) return;
+
+		var data = JSON.parse(xhr.responseText);
+		callback(data);
+	};
+
+	xhr.open('GET', url, true);
+	xhr.send();
+}
+
 function insertRepoAge() {
 	var url = window.location.pathname;
 	var result = /^\/[^\/]+\/[^\/]+/.exec(url);
@@ -7,23 +29,16 @@ function insertRepoAge() {
 	var repoName = document.querySelector('.repohead .entry-title strong');
 	if (!repoName) return;
 
-	var xhr = new XMLHttpRequest();
-	xhr.onreadystatechange = function () {
-		if (xhr.readyState != 4) return;
+		getRepoAge(url, function (data) {
+			var age = data[0].weeks[0].w;
+			age = window.moment.unix(age).fromNow(true);
 
-		var data = JSON.parse(xhr.responseText);
-		var age = data[0].weeks[0].w;
-		age = window.moment.unix(age).fromNow(true);
-
-		var repoAge = document.createElement('span');
-		repoAge.textContent = ' (' + age + ')';
-		repoAge.style.fontSize = '16px';
-		repoAge.style.color = '#aaa';
-		repoName.parentNode.insertBefore(repoAge, repoName.nextSibling);
-	};
-
-	xhr.open('GET', url, true);
-	xhr.send();
+			var repoAge = document.createElement('span');
+			repoAge.textContent = ' (' + age + ')';
+			repoAge.style.fontSize = '16px';
+			repoAge.style.color = '#aaa';
+			repoName.parentNode.insertBefore(repoAge, repoName.nextSibling);
+		});
 }
 
 document.addEventListener('DOMContentLoaded', insertRepoAge);
